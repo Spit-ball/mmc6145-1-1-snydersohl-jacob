deef39d1fb2e7e52af0da7d2bc7de7b4fcae9f68
The Spitball <83725076+Spit-ball@users.noreply.github.com>
2023-09-03 19:21:08 -0500

    testing solution

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -38,6 +38,7 @@ export default function Home() {
         <span className={`neon-flicker ${styles["neon-flicker"]}`}>s</span>
       </h1>
       <Project />
+      <Project />
     </main>
   );
 }
